6585a544e6500a6ae049a78ccfa9ecb478b0c9a2
TWQ1765 <www.taaadaaa@qq.com>
2018-10-27 19:21:52 +0800

    still need to test...

---
+++ src/Mouse_in_a_Maze.h
@@ -0,0 +1,5 @@
+#ifndef _MOUSE_IN_A_MAZE_H
+#define _MOUSE_IN_A_MAZE_H
+
+
+#endif // _MOUSE_IN_A_MAZE_H
---
+++ test/test_Mouse_in_a_Maze.c
@@ -0,0 +1,15 @@
+#include "unity.h"
+#include "Mouse_in_a_Maze.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_Mouse_in_a_Maze_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement Mouse_in_a_Maze");
+}
